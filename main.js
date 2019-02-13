889f6ed4c39558ed907ad36e16affbfc4c6d874e
makkelaig <makkelaig@gmail.com>
2019-02-13 17:00:45 +0100

    Delete test files

--- main.js
+++
@@ -1,8 +0,0 @@
-// Get references to UI elements
-let connectButton = document.getElementById('connect');
-let disconnectButton = document.getElementById('disconnect');
-let terminalContainer = document.getElementById('terminal');
-let sendForm = document.getElementById('send-form');
-let inputField = document.getElementById('input');
-
-
